0d73aae47c99d1c21a9dc027a1c9f1280fe18316
Amanda Tuyen <atuyen26@gmail.com>
2021-04-14 22:19:45 +0000

    add in search function queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -22,14 +22,14 @@ const getUserWithEmail = function(email) {
   SELECT *
   FROM users
   WHERE email = $1;`, [email])
-  .then(res => {
-    if (res.rows.length === 0) {
-      return null;
-    }
-    console.log('userwithemail:', res.rows[0])
-    return Promise.resolve(res.rows[0]);
-  })
-}
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null;
+      }
+      // console.log('userwithemail:', res.rows[0])
+      return Promise.resolve(res.rows[0]);
+    });
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -42,15 +42,15 @@ const getUserWithId = function(id) {
   SELECT *
   FROM users
   WHERE id =$1;`, [id])
-  .then(res => {
-    if (res.rows.length === 0) {
-      return null;
-    }
-    console.log('getuserwithID:', res.rows[0]);
-    return Promise.resolve(res.rows[0]);
-  })
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null;
+      }
+      // console.log('getuserwithID:', res.rows[0]);
+      return Promise.resolve(res.rows[0]);
+    });
 
-}
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -65,11 +65,11 @@ const addUser =  function(user) {
   VALUES ($1, $2, $3)
   RETURNING *;
   `, [user.name, user.email, user.password])
-  .then(res => {
-    console.log('addUser:', res.rows[0]);
-    return Promise.resolve(res.rows[0]);
-  })
-}
+    .then(res => {
+    // console.log('addUser:', res.rows[0]);
+      return Promise.resolve(res.rows[0]);
+    });
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -91,12 +91,12 @@ const getAllReservations = function(guest_id, limit = 10) {
   ORDER BY start_date
   LIMIT $2;
   `, [guest_id, limit])
-  .then(res => {
-    console.log('reservations:', res.rows);
-    return Promise.resolve(res.rows);
-  });
+    .then(res => {
+    // console.log('reservations:', res.rows);
+      return Promise.resolve(res.rows);
+    });
   
-}
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -109,16 +109,73 @@ exports.getAllReservations = getAllReservations;
  */
 
 const getAllProperties = function(options, limit = 10) {
-const queryString =`
-SELECT *
-FROM properties
-LIMIT $1;
-`;
-const values = [limit];
-return pool.query(queryString, values)
-.then(res => res.rows);
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
 
-}
+  if (options.owner_id) {
+    if (queryParams.length === 0) {
+      queryString += 'WHERE';
+    }
+    if (queryParams.length > 0) {
+      queryString += 'AND';
+    }
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    if (queryParams.length === 0) {
+      queryString += 'WHERE';
+    }
+    if (queryParams.length > 0) {
+      queryString += 'AND';
+    }
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryString += ` cost_per_night >= $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    if (queryParams.length === 0) {
+      queryString += 'WHERE';
+    }
+    if (queryParams.length > 0) {
+      queryString += 'AND';
+    }
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += ` cost_per_night <= $${queryParams.length}`;
+  }
+
+  if (options.minimum_rating) {
+    if (queryParams.length === 0) {
+      queryString += 'WHERE';
+    }
+    if (queryParams.length > 0) {
+      queryString += 'AND';
+    }
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` property_reviews.rating >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log('string:', queryString,'params:', queryParams);
+  return pool.query(queryString, queryParams)
+    .then(res => res.rows);
+};
 
 
 exports.getAllProperties = getAllProperties;
@@ -134,27 +191,9 @@ const addProperty = function(property) {
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
   RETURNING *;
   `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, (property.cost_per_night * 100), property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
-  .then(res => {
-    console.log('property:', res.rows[0]);
-   return Promise.resolve(res.rows[0]); 
-  })
-}
+    .then(res => {
+      console.log('property:', res.rows[0]);
+      return Promise.resolve(res.rows[0]);
+    });
+};
 exports.addProperty = addProperty;
-
-// INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, country, street, city, province, post_code)
-// VALUES (3, 'The Hobbit Hole', 'description', 'https://a0.muscache.com/im/pictures/924798e4-2fe1-47de-9636-947332829bcc.jpg?im_w=960', 'https://a0.muscache.com/im/pictures/924798e4-2fe1-47de-9636-947332829bcc.jpg?im_w=960', '100', 'Canada', 'Main St', 'Vancouver', 'BC', 'V5U 8M9'),
-// (1, 'The Cozy Cottage', 'description', 'https://a0.muscache.com/im/pictures/712a31dd-fad0-4882-ba33-783e5e8620e4.jpg?im_w=960', 'https://a0.muscache.com/im/pictures/712a31dd-fad0-4882-ba33-783e5e8620e4.jpg?im_w=960', '100', 'Canada', 'Cottage St', 'Vancouver', 'BC', 'V5U 8M9'),
-// (2, 'The Mermaids Cove', 'description', 'https://a0.muscache.com/im/pictures/d326e402-0a38-4dfd-b42c-0ace66c0921f.jpg?im_w=960', 'https://a0.muscache.com/im/pictures/d326e402-0a38-4dfd-b42c-0ace66c0921f.jpg?im_w=960', '100', 'Canada', 'Water St', 'Vancouver', 'BC', 'V5U 8M9');
-
-
-// const addUser =  function(user) {
-//   pool.query(`
-//   INSERT INTO users (name, email, password)
-//   VALUES ($1, $2, $3)
-//   RETURNING *;
-//   `, [user.name, user.email, user.password])
-//   .then(res => {
-//     console.log('addUser:', res.rows[0]);
-//     return Promise.resolve(res.rows[0]);
-//   })
-// }
